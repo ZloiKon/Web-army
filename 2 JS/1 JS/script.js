9bf7083230bfe4e3753df76c869e664325b96413
ZloiKon <maks.miron0v@yandex.ru>
2020-08-24 02:19:59 +0300

    2 Занятие

--- 2 JS/1 JS/script.js
+++ 2 JS/1 JS/script.js
@@ -1,11 +1,24 @@
 
-result.innerHTML = "Вот тебе текст от JS";
+function setResult(event){
+    event.target
+alert(event.target.value)
+setResult.innerHTML = event.target.value
+
+}
+
+btn1.addEventListener("click", setResult)
+btn2.addEventListener("click", setResult)
+btn3.addEventListener("click", setResult)
+btn4.addEventListener("click", setResult)
+
+function setResult(event){
+result.innerHTML = txtB.value;
 
 
 
 
-
-
+}
+btnB.addEventListener("click", setResult)
 
 
 
