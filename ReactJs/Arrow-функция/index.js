f8b0769e63a7e03585f361a30c047f2a9fec0bee
ZloiKon <maks.miron0v@yandex.ru>
2021-01-31 02:40:38 +0300

    31.01.2021

--- ReactJs/Arrow-функция/index.js
+++ ReactJs/Arrow-функция/index.js
@@ -1,13 +1,5 @@
-let numbers = [240, 26, 420, 343, 997, 536, 268, 950, 457, 341, 912, 923, 883, 14, 253, 79, 264, 55, 510, 790, 330, 321, 764, 252, 30, 626, 983, 261, 617, 18]
 
-numbers = numbers.filter((item, i)=>{
-    return item % 2;
-});
+let serverData = [24, "User", false, 85, -5, 0, 45, "google.com"]
 
-numbers = nunbers.map((item, i)=>{
-    return item*10;
-});
-
-console.log(numbers);
-
-
+let [User,google.com] = [[serverData.filter((item, i)=>{return typeof item =="boolean"})]  ,  [serverData.filter((item, i)=>{return typeof item =="number"})]  ,  [serverData.filter((item, i)=>{return typeof item =="false"})]]
+console.log(...serverData)
